614bcd8b2632ac0d475b12cd43168882d3d48ea0
sjf <sarah.fortune@gmail.com>
2010-03-24 15:41:10 -0700

    Initial import of ./bilinear.cuh

---
+++ bilinear.cuh
@@ -0,0 +1,20 @@
+/*
+ * simpleTexture.cuh
+ *
+ *  Created on: 3 Feb 2010
+ *      Author: sjf65
+ */
+
+#ifndef BILINEAR_CUH_
+#define BILINEAR_CUH_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+void cuda_bilinear(img *image);
+
+#ifdef __cplusplus
+ }
+#endif
+#endif /* BILINEAR_CUH_ */
