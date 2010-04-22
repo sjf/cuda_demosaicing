2e5c54dd5d214ac16e708c466320aa8bfbc3e479
sjf <sarah.fortune@gmail.com>
2010-04-22 06:56:44 -0700

    Initial import of ./bilinear.h

---
+++ bilinear.h
@@ -0,0 +1,12 @@
+/*
+ * bilinear.h
+ *
+ *  Created on: 22 Apr 2010
+ *      Author: sjf
+ */
+
+#ifndef BILINEAR_H_
+#define BILINEAR_H_
+void host_bilinear(img *image);
+
+#endif /* BILINEAR_H_ */
