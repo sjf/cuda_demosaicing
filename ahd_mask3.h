657bf75c090089d0806232f8b1cf9458f607fbc0
sjf <sarah.fortune@gmail.com>
2010-05-15 11:52:00 -0700

    Initial import of ./ahd_mask3.h

---
+++ ahd_mask3.h
@@ -0,0 +1,12 @@
+/*
+ * ahd_mask2.h
+ *
+ *  Created on: 14 May 2010
+ *      Author: sjf
+ */
+
+#ifndef AHD_MASK3_H_
+#define AHD_MASK3_H_
+void mask_ahd3(img *image);
+
+#endif /* AHD_MASK3_H_ */
