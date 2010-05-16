1986d760029a0ed298201bb86d28a7625cfa89d7
sjf <sarah.fortune@gmail.com>
2010-05-16 08:27:58 -0700

    Initial import of ./ahd_mask2.h

---
+++ ahd_mask2.h
@@ -0,0 +1,12 @@
+/*
+ * ahd_mask2.h
+ *
+ *  Created on: 14 May 2010
+ *      Author: sjf
+ */
+
+#ifndef AHD_MASK2_H_
+#define AHD_MASK2_H_
+void mask_ahd2(img *image);
+int dist3(int a, int b, int c, int a1, int b1, int c1);
+#endif /* AHD_MASK2_H_ */
