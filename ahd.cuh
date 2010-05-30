5f4adad618e4ca8d0245739838826a87d0a8a7d6
sjf <sarah.fortune@gmail.com>
2010-05-30 13:50:56 -0700

    Initial import of ./ahd.cuh

---
+++ ahd.cuh
@@ -0,0 +1,16 @@
+/*
+ * simpleTexture.cuh
+ *
+ *  Created on: 3 Feb 2010
+ *      Author: sjf65
+ */
+
+#ifndef AHD_CUH_
+#define AHD_CUH_
+
+#define P 16
+#define pixel4 char4
+
+void cuda_ahd(img *image);
+
+#endif /* AHD_CUH_ */
